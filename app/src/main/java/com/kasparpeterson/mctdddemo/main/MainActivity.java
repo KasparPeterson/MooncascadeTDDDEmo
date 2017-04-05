896b89961febded95f8a3ec8c5c6eb0fe10e6b8b
KasparPeterson <kasparpeterson@hotmail.com>
2017-04-05 10:37:58 +0300

    Removes implementation and keeps just an empty template

--- app/src/main/java/com/kasparpeterson/mctdddemo/main/MainActivity.java
+++ app/src/main/java/com/kasparpeterson/mctdddemo/main/MainActivity.java
@@ -1,8 +1,5 @@
 package com.kasparpeterson.mctdddemo.main;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,9 +7,5 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MainActivity extends AppCompatActivity {
-
-    public static Intent getIntent(@NonNull Context context) {
-        return new Intent(context, MainActivity.class);
-    }
 
 }
--- app/src/main/java/com/kasparpeterson/mctdddemo/signup/SignUpMVP.java
+++ app/src/main/java/com/kasparpeterson/mctdddemo/signup/SignUpMVP.java
@@ -1,10 +1,5 @@
 package com.kasparpeterson.mctdddemo.signup;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-
-import com.kasparpeterson.mctdddemo.api.FailedReason;
-import com.kasparpeterson.mctdddemo.api.SignUpResult;
 import com.kasparpeterson.simplemvp.MVPBaseModel;
 import com.kasparpeterson.simplemvp.MVPBasePresenter;
 import com.kasparpeterson.simplemvp.MVPBasePresenterModelOperations;
@@ -18,8 +13,7 @@ public interface SignUpMVP {
 
     // Presenter -> View
     interface ViewOperations extends MVPBaseViewOperations {
-        void showView(@NonNull SignUpViewState viewState);
-        void openMainActivity();
+
     }
 
     // View -> Presenter
@@ -30,18 +24,16 @@ public interface SignUpMVP {
             super(view, model);
         }
 
-        abstract void onSignUp(@Nullable String name, @Nullable String password);
     }
 
     // Model -> Presenter
     interface PresenterModelOperations extends MVPBasePresenterModelOperations {
-        void onSignUpSucceeded(@NonNull SignUpResult signUpResult);
-        void onSignUpFailed(@NonNull FailedReason failedReason);
+
     }
 
     // Presenter -> Model
     abstract class ModelOperations extends MVPBaseModel<PresenterModelOperations> {
-        abstract void signUp(@NonNull String name, @NonNull String password);
+
     }
 
 }
